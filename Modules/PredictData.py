4f25a9fe7dc530cb77fe64a19c32da9d342376bb
Ganappriyan C <ganappriyanc@gmail.com>
2023-03-18 07:37:25 +0530

    Model Updated

---
+++ Modules/PredictData.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import cv2
+import os
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+
+class PredictData:
+    def __init__(self) -> None:
+        # Load Model
+        
+    
+    def checkImage():
+        
+        return
