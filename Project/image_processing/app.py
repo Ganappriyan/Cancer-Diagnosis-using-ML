6c864e0cd4824f3423e99c875d63d8beee1268a0
Ganappriyan C <ganappriyanc@gmail.com>
2023-04-05 12:48:49 +0530

    TMP

--- Project/image_processing/app.py
+++ Project/image_processing/app.py
@@ -4,21 +4,23 @@ from cnnmodel import CNNModel
 
 app = FastAPI()
 
-classification_model = CNNModel(modelname="classification", filename="lenet.h5")
-grading_model = CNNModel(modelname="grade", filename="simple_softmax.h5")
+# classification_model = CNNModel(modelname="classification", filename="lenet.h5")
+# grading_model = CNNModel(modelname="grade", filename="simple_softmax.h5")
 
 @app.post("/process/")
 async def classify_image(file: UploadFile = File(...)):
     img_bytes = await file.read()
-    return {'classification': classification_model.predict(img_bytes), 
-            'grading': grading_model.predict(img_bytes)}
+    # return {'classification': classification_model.predict(img_bytes), 
+    #         'grading': grading_model.predict(img_bytes)}
+    return {'classification': "{'cervix_dyk': 0.0, 'cervix_koc': 0.0, 'cervix_mep': 1.0, 'cervix_pab': 0.0, 'cervix_sfi': 0.0}",
+            'grading': ""}
 
-@app.post("/classify/")
-async def classify_image(file: UploadFile = File(...)):
-    return classification_model.predict(file.file)
+# @app.post("/classify/")
+# async def classify_image(file: UploadFile = File(...)):
+#     return classification_model.predict(file.file)
 
-@app.post("/grade/")
-async def grade_images(file: UploadFile = File(...)):
-    img_bytes = await file.read()
-    return grading_model.predict(img_bytes)
+# @app.post("/grade/")
+# async def grade_images(file: UploadFile = File(...)):
+#     img_bytes = await file.read()
+#     return grading_model.predict(img_bytes)
     
